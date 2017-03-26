6582581e255d3e0f2c53f0aec887e6710f2e5cf3
Michael Andrade <mandra71@gmail.com>
2017-03-26 18:37:52 +0000

    Starting out with just the title update via javascript

--- scratchpad.js
+++ scratchpad.js
@@ -1,3 +1,4 @@
 
-//just starting out, heading out of town for spring break. I wont be back until Saturday the 11th. 
-//I just got back and am beginning my script
+//var doc = document.implementation.createDocument("", "", null);
+
+document.title = "Javascript Assignment";
